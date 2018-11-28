63588e92d01364bfa8a56ecf58512bb986749223
sasrous <n.casascrous@gmail.com>
2018-11-28 03:09:52 +0100

    make it better

--- myapp/app.js
+++ myapp/app.js
@@ -4,15 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
-const mongoose = require('mongoose');
-
-const dbName = 'celebrities-db';
-mongoose.connect(`mongodb://localhost/${dbName}`);
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const celebritiesRouter = require ('./routes/celebrities');
-
 
 const app = express();
 
@@ -27,23 +20,21 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/celebrities', celebritiesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,10 +1,18 @@
+const express = require('express');
+const router = express.Router();
 
- const express = require('express');
- const router = express.Router();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('index', { title: 'Index' });
+});
+router.get('/contact', function(req, res, next) {
+	res.render('contact', { title: 'contact' });
+});
+router.get('/about', function(req, res, next) {
+	res.render('about', { title: 'about' });
+});
+router.get('/work', function(req, res, next) {
+	res.render('work', { title: 'work' });
+});
 
- /* GET home page. */
- router.get('/', function(req, res, next) {
-   res.render('index', { title: 'My celebrity list' });
- });
-
- module.exports = router;
+module.exports = router;
